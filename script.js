25fe46ce861bfbaaaf8a45ed452ecd754a9b285e
jordanoboyle <jmcmcode@gmail.com>
2024-11-18 12:58:33 -0700

    two different ways to display the API lyric song data

--- script.js
+++ script.js
@@ -28,19 +28,50 @@ async function searchSongs(term) {
 function showData(data) {
   let output = '';
 
-  data.data.forEach(song => {
-    output += `
-      <li>
-        <span><strong>${song.artist.name} </strong> - ${song.title}</span>
-        <button 
-        class="btn" 
-        data-artist= "${song.artist.name}"
-        data-songtitle="${song.title}"> 
-        Get Lyrics 
-        </button>
-      </li>
-    `
-  })
+  //THIS IS ONE WAY
+  // data.data.forEach(song => {
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name} </strong> - ${song.title}</span>
+  //       <button 
+  //       class="btn" 
+  //       data-artist= "${song.artist.name}"
+  //       data-songtitle="${song.title}"> 
+  //       Get Lyrics 
+  //       </button>
+  //     </li>
+  //   `;
+  // });
+  
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </u>
+  // `
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map(song => `
+        <li>
+         <span><strong>${song.artist.name} </strong> - ${song.title}</span>
+         <button 
+         class="btn" 
+         data-artist= "${song.artist.name}"
+         data-songtitle="${song.title}"> 
+         Get Lyrics 
+         </button>
+       </li>
+        `).join(" ")
+      };
+    </ul>
+  `;
+
+  // if (data.prev || data.next) {
+  //   more.innerHTML = `
+  //     ${data.prev ? '<button class="btn"> Previous </button>' : ''}
+  //     ${data.next ? '<button class="btn"> Previous </button>' : ''}
+  //   `;
+  // }
 }
 
 //Event Listeners
