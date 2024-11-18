2dbbe0fe7017aea6fdb1e09c334036f4b4eb6ae0
jordanoboyle <jmcmcode@gmail.com>
2024-11-18 12:37:31 -0700

    Get request for lyrics logged to console

--- script.js
+++ script.js
@@ -5,10 +5,42 @@ const result = document.getElementById('result');
 const more   = document.getElementById('more');
 
 //API URL NEEDED
-const apiURL = 'https://api.lyrics.ovh/v1';
+const apiURL = 'https://api.lyrics.ovh';
 
-function searchSongs(terms) {
-  return true
+//SEARCH SONG BY SONG OR ARTIST
+
+//A WAY TO DO IT WITH .then
+// function searchSongs(term) {
+//   fetch(`${apiURL}/suggest/${term}`)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// }
+//USING ASYNC (this is cleaner and easier to read)
+async function searchSongs(term) {
+  const response = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await response.json();
+  // console.log(data)
+
+  showData(data);
+}
+
+//Show Song and Artists in DOM
+function showData(data) {
+  let output = '';
+
+  data.data.forEach(song => {
+    output += `
+      <li>
+        <span><strong>${song.artist.name} </strong> - ${song.title}</span>
+        <button 
+        class="btn" 
+        data-artist= "${song.artist.name}"
+        data-songtitle="${song.title}"> 
+        Get Lyrics 
+        </button>
+      </li>
+    `
+  })
 }
 
 //Event Listeners
